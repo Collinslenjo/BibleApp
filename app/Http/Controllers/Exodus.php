4065ad9418388d444149361d50a644a60824f434
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-14 21:34:39 +0300

    rexd

--- app/Http/Controllers/Exodus.php
+++ app/Http/Controllers/Exodus.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class Exodus extends Controller
 {
-    //
+    public function index()
+    {
+	  $myrawdata = file_get_contents(__DIR__.'/data/books/1-gen.json',true);
+	  $json_a=json_decode($myrawdata,true);
+	  return view('exodus',['json_a'=>$json_a]);
+    }
 }
