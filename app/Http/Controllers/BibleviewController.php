cba111f61eadebe5536d4d4613e24d8df4fa8795
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-15 12:00:37 +0300

    genesis controller

--- app/Http/Controllers/BibleviewController.php
+++ app/Http/Controllers/BibleviewController.php
@@ -8,6 +8,10 @@ class BibleviewController extends Controller
 {
     public function index()
     {
+	  return view('welcome');
+    }
+    public function genesis()
+    {
 	  $myrawdata = file_get_contents(__DIR__.'/data/books/1-gen.json',true);
 	  $json_a=json_decode($myrawdata,true);
 	  return view('welcome',['json_a'=>$json_a]);
