16907b633fdb1ece748879560651d76e67a73bb6
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-16 23:57:15 +0300

    bkin12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,6 +33,21 @@
                 <h2>Holy Bible</h2>
                 <p>Still in development</p>
 
+                
+                <h1>{{$json_a['chapters'][0][0]['bookname']}}</h1>
+                @foreach($json_a['chapters'] as $play)
+                <div class="panel-body">
+                @foreach($play as $p)
+                <ul class="list-unstyled activity-list">
+                <li>
+                {{$p['chapter']}} : {{$p['verse']}} <br/> 
+                {{$p['text']}}<br/>
+                </li>
+                </ul>
+                @endforeach
+                </div>
+                @endforeach
+
             </div>
         </div>
     </body>
