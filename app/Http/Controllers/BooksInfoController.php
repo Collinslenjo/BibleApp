d9d50e2f15452f74181f2edad2c0192c65ae5291
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-16 23:54:58 +0300

    bkin

--- app/Http/Controllers/BooksInfoController.php
+++ app/Http/Controllers/BooksInfoController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class BooksInfoController extends Controller
 {
-    //
+    $myrawdata = file_get_contents(__DIR__.'/data/books/1-gen.json',true);
+	$json_a=json_decode($myrawdata,true);
+	return view('genesis',['json_a'=>$json_a])
 }
