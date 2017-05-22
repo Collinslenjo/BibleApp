079045d278ec3986461b368c59fc85266a861e07
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-22 21:50:35 +0300

    leviticus controller edited the right way

--- app/Http/Controllers/BibleviewController.php
+++ app/Http/Controllers/BibleviewController.php
@@ -19,7 +19,7 @@ class BibleviewController extends Controller
 
     public function leviticus()
     {
-      $myrawdata = file_get_contents(__DIR__.'/data/books/1-lev.json',true);
+      $myrawdata = file_get_contents(__DIR__.'/data/books/3-lev.json',true);
       $json_a=json_decode($myrawdata,true);
       return view('leviticus',['json_a'=>$json_a]);
     }
