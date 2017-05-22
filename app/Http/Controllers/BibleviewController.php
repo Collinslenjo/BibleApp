abd9cc6ceae878cf8fb5679da65b4e449f1b81be
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-22 21:47:42 +0300

    leviticus controller

--- app/Http/Controllers/BibleviewController.php
+++ app/Http/Controllers/BibleviewController.php
@@ -16,4 +16,11 @@ class BibleviewController extends Controller
 	  $json_a=json_decode($myrawdata,true);
 	  return view('genesis',['json_a'=>$json_a]);
     }
+
+    public function leviticus()
+    {
+      $myrawdata = file_get_contents(__DIR__.'/data/books/1-lev.json',true);
+      $json_a=json_decode($myrawdata,true);
+      return view('leviticus',['json_a'=>$json_a]);
+    }
 }
