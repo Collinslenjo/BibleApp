f5a93cae85e7ba675d17e65fa9b059c00f8d98a8
collins lenjo <collinslenjo.cl@gmail.com>
2017-06-04 20:45:54 +0300

    Addition of controller deutronomy

--- app/Http/Controllers/BibleviewController.php
+++ app/Http/Controllers/BibleviewController.php
@@ -28,4 +28,9 @@ class BibleviewController extends Controller
       $json_a=json_decode($myrawdata,true);
       return view('numbers',['json_a'=>$json_a]);
     }
+    public function deutronomy(){
+      $myrawdata = file_get_contents(__DIR__.'/data/books/5-deu.json',true);
+      $json_a=json_decode($myrawdata,true);
+      return view('deutronomy',['json_a'=>$json_a]);
+    }
 }
