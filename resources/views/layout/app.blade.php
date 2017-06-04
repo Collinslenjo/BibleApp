ade96796d24160e805566b6e7fd4023a94eb296c
collins lenjo <collinslenjo.cl@gmail.com>
2017-06-04 21:11:06 +0300

    Last of 4th of sunday

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,27 +11,35 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-    <!-- My navigation starts here -->
-        <div class="flex-center position-ref full-height">
-            <div class="nav">
-                <nav>
-                     <ul class="top-right links">
-                        <li><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="{{ url('/genesis') }}">Genesis</a></li>
-                        <li><a href="{{ url('/exodus') }}">Exodus</a></li>
-                        <li><a href="{{ url('/leviticus') }}">Leviticus</a></li>
-                        <li><a href="{{ url('/numbers') }}">Numbers</a></li>
-                        <li><a href="{{ url('/deutronomy') }}">Deutronomy</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        <!-- My navigation ends here -->
 
-<div class="wrapper">
-    @yield('content')
+    <!-- My navigation starts here -->
+<div class="container">
+        <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" ><a href="{{ url('/genesis') }}">Genesis</a></li>
+            <li role="presentation" ><a href="{{ url('/exodus') }}">Exodus</a></li>
+            <li role="presentation" ><a href="{{ url('/leviticus') }}">Leviticus</a></li>
+            <li role="presentation" ><a href="{{ url('/numbers') }}">Numbers</a></li>
+            <li role="presentation" ><a href="{{ url('/deutronomy') }}">Deutronomy</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><a href="{{ url('/') }}">Holy Bible</a></h3>
+    </div>
 </div>
 
+        <!-- My navigation ends here -->
+
+        <!-- Let all the content be viewed here -->
+        <div class="wrapper">
+
+            @yield('content')
+
+        </div>
+        <!-- Content ended -->
+
+<!-- JavaScript files Come here -->
 <script src="{{ asset('js/app.js') }}"></script>
+
     </body>
 </html>
